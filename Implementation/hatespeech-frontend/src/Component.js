4b36863b9488452f6026a4b50ea6c173fd95b27f
aboo212031 <abubakrryzan@gmail.com>
2023-03-09 21:14:40 +0530

    UI update

--- Implementation/hatespeech-frontend/src/Component.js
+++ Implementation/hatespeech-frontend/src/Component.js
@@ -1,51 +1,75 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Button, TextField } from "@material-ui/core";
+// import { Button, TextField } from "@material-ui/core";
 
 const api = axios.create({
-	baseURL: "http://127.0.0.1:5000", // replace with your base URL
+  baseURL: "http://127.0.0.1:5000", // replace with your base URL
 });
 
 const MyComponent = () => {
-	const [inputText, setInputText] = useState("");
-	const [responseText, setResponseText] = useState("");
+  const [inputText, setInputText] = useState("");
+  const [responseText, setResponseText] = useState("");
 
-	const handleSubmit = async () => {
-		try {
-			const response = await api.post("/api/input", { text: inputText });
-			setResponseText(response.data[0]);
-		} catch (error) {
-			console.error(error);
-		}
-	};
+  const handleSubmit = async () => {
+    try {
+      if (inputText != "") {
+        const response = await api.post("/api/input", { text: inputText });
+        setResponseText(response.data[0]);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-	const handleInputChange = (event) => {
-		setInputText(event.target.value);
-	};
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+    if (event.target.value == "") {
+      document.body.style.backgroundColor = "white";
+      setResponseText(null);
+    }
+  };
 
-	useEffect(() => {
-		if (responseText && responseText === "Hate Speech") {
-			document.body.style.backgroundColor = "red";
-		} else if (responseText && responseText === "Non-Hate Speech") {
-			document.body.style.backgroundColor = "green";
-		} else {
-			document.body.style.backgroundColor = "white";
-		}
-	}, [responseText]);
+  useEffect(() => {
+    if (responseText && responseText === "Hate Speech") {
+      document.body.style.backgroundColor = "red";
+    } else if (responseText && responseText === "Non-Hate Speech") {
+      document.body.style.backgroundColor = "green";
+    } else {
+      document.body.style.backgroundColor = "white";
+    }
+  }, [responseText]);
 
-	return (
-		<div>
-			<TextField
-				id="outlined-basic"
-				label="Enter Text"
-				variant="outlined"
-				onChange={handleInputChange}
-			/>
-			<Button onClick={handleSubmit} variant="contained" color="primary">
-				Submit
-			</Button>
-		</div>
-	);
+  return (
+    <div>
+      <div className="d-inline-flex align-items-center">
+        <img id="logo" src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" />
+        <h2>BitsPlease</h2>
+      </div>
+      <h2 className="d-flex justify-content-center">
+        Let's Prevent Spreading Hate Speech
+      </h2>
+      <div class="d-flex justify-content-center">
+        <div class="form-floating w-25 input-group rounded">
+          <input
+            type="text"
+            class="form-control"
+            id="floatingInput"
+            placeholder="Enter Comment...."
+            onChange={handleInputChange}
+          />
+          <label for="floatingInput">Comment...</label>
+          <button type="button" class="btn btn-primary" onClick={handleSubmit}>
+            <i className="fa fa-search"></i>
+          </button>
+        </div>
+      </div>
+      {responseText ? (
+        <p id="marginAdder" class="d-flex justify-content-center">
+          {responseText}
+        </p>
+      ) : null}
+    </div>
+  );
 };
 
 export default MyComponent;
