a98dd5b072ce8ba7b074ec11452b37dda7f3f551
Aakif Ahmed <aakifahmedd@gmail.com>
2023-03-31 08:07:07 +0530

    Update App.test.js

--- Implementation/hatespeech-frontend/src/App.test.js
+++ Implementation/hatespeech-frontend/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+
+// Defining a test using the testing library
 test('renders learn react link', () => {
-  render(<App />);
+  render(<App />); // Render the 'App' component
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
